f9cd497a7141a6e9e655d31b3f095d14a468567b
Ahmed Musallam <atyaf66@gmail.com>
2018-03-05 19:53:27 -0600

    add logo and favicons

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <div class="main container m-2">
         <nav class="navbar p-2">
           <section class="navbar-section">
+            <img class="logo" src="static/logo.svg">
             <router-link class="btn btn-link" to="/">Home</router-link>
             <router-link class="btn btn-link" to="/motivation">Motivation</router-link>
             <a class="btn btn-link" href="https://github.com/ahmed-musallam/rotten-tomato-game-tracker">source code</a>
@@ -26,6 +27,9 @@ export default {
 @import '../node_modules/spectre.css/dist/spectre.min.css';
 @import '../node_modules/spectre.css/dist/spectre-icons.min.css';
 
+.logo {
+  height: 30px;
+}
 .fade-enter-active, .fade-leave-active {
   transition: opacity 0.5s;
 }
@@ -35,7 +39,6 @@ export default {
   width:0px;
   overflow: hidden;
 }
-
 .main {
     min-height: calc(100vh - 80px);
 }
